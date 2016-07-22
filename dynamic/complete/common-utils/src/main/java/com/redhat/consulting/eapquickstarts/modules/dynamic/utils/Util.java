9be968400ebc29ad6226023078ecec932202d272
Bryan Parry <bparry@redhat.com>
2016-07-22 11:05:15 -0400

    add comment on util method

--- dynamic/complete/common-utils/src/main/java/com/redhat/consulting/eapquickstarts/modules/dynamic/utils/Util.java
+++ dynamic/complete/common-utils/src/main/java/com/redhat/consulting/eapquickstarts/modules/dynamic/utils/Util.java
@@ -2,6 +2,12 @@ package com.redhat.consulting.eapquickstarts.modules.dynamic.utils;
 
 public class Util 
 {
+    /**
+     * A contrived example to help demonstrate class loading with JBoss Modules.
+     * 
+     * @param string
+     * @return
+     */
     public static String capitalize(String string)
     {
         if (string == null)
